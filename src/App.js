7a57e2b7a15b848731785d05e5206ae2b5d3628b
Tony Ojeda <tojeda96@gmail.com>
2020-12-13 08:29:22 -0500

    feature: add component card-list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {Component} from 'react';
+
+import {CardList} from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -12,14 +14,17 @@ class App extends Component {
 	componentDidMount(){
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
-			.then(users => this.setState({monsters:users}))
-	}
+		.then(users => this.setState({monsters:users}))
+}
 
-	render() {
-		return (
-    		<div className="App">
+render() {
+	return (
+		<div className="App">
+			<CardList name='Tony' >	
 				{this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1> )}
-    		</div>
+			</CardList>
+		
+    	</div>
 		)
 	}
 } 
